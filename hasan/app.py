b0a20eaf3189ec55aafab1384b67ee0a5ee99107
Atique7865 <hasanmohammadatique786@gmail.com>
2026-06-23 11:49:21 +0600

    second commit

--- hasan/app.py
+++ hasan/app.py
@@ -1,16 +1,14 @@
-import os
-import sys
-
-
 def hello():
     x = 10
-    y = 20
     print("Hello World")
     return x
 
 
-def add(a,b):
-  return a+b
+def add(a, b):
+    return a + b
 
+
+result = add(5, 10)
+print(result)
 
 hello()
